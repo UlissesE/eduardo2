0db2d6597d5c0ce62bf7a113f0cd021594262e8e
Eduardo Ulisses <eduardoulisses2006@gmail.com>
2025-03-11 20:19:11 -0300

    Revert "Corrigir narração"
    This reverts commit 9a4ed3c6e9a33066a5e15ba65dcadbff7dc01bac.

--- app.js
+++ app.js
@@ -65,11 +65,9 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
-if ('speechSynthesis' in window) {
-    let utterance = new SpeechSynthesisUtterance(texto);
-    utterance.lang = 'pt-BR';
-    utterance.rate = 1.2;
-    window.speechSynthesis.speak(utterance);
-} else {
-console.log("Web Speech API não suportada neste navegador.");
-}
+
+
+
+
+
+
